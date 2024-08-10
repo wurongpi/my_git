fed555af682b74460281213c2969e04dcd71de62
wupi <wurongpi@gmail.com>
2024-08-10 16:11:53 +0800

    step 5

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 from . import data
 
@@ -18,6 +19,14 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
 
@@ -29,3 +38,8 @@ def init(args):
 def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
